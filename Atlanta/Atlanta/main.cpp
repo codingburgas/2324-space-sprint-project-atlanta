fb6c1c43746ba59d4f1228fc143743b53a7e189b
ZSVladkov22 <zsvladkov22@codingburgas.bg>
2024-02-21 17:19:05 +0200

    Add Start button start screen

--- Atlanta/Atlanta/main.cpp
+++ Atlanta/Atlanta/main.cpp
@@ -1,11 +1,65 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
+#include "startMenu.h"
 using namespace sf;
+using namespace std;
     
 int main()
 {
-    RenderWindow window(VideoMode(800, 800), "Atlanta");
-    window.setFramerateLimit(60);
+    RenderWindow window;
 
-    return 0;
+    Vector2i centerWindow((VideoMode::getDesktopMode().width / 2) - 445, (VideoMode::getDesktopMode().height / 2) - 480);
+
+    window.create(VideoMode(900, 900), "Atlanta", Style::Titlebar | Style::Close);
+    window.setPosition(centerWindow);
+
+    window.setKeyRepeatEnabled(true);
+
+    Font arial;
+    arial.loadFromFile("arial.ttf");
+    
+    startButton btn1("Start", { 200, 50 }, 20, Color::Green, Color::Black);
+    btn1.setPosition({ 350, 300 });
+    btn1.setFont(arial);
+
+
+    while (window.isOpen())
+    {
+        Event event;
+        if (Keyboard::isKeyPressed(Keyboard::Return)) {
+            
+        }
+        else if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+
+        }
+
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case Event::Closed:
+                window.close();
+            case Event::MouseMoved:
+                if (btn1.isMouseOver(window)) {
+                    btn1.setBackColor(Color::White);
+                }
+                else {
+                    btn1.setBackColor(Color::Green);
+                }
+                break;
+            case Event::MouseButtonPressed:
+                if (btn1.isMouseOver(window)) {
+                    cout << "Test";
+                }
+
+            }
+        }
+
+        window.clear();
+        btn1.drawTo(window);
+        window.display();
+    }
+
 }
--- Atlanta/Atlanta/startMenu.h
+++ Atlanta/Atlanta/startMenu.h
@@ -1,1 +1,78 @@
 #pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+class startButton
+{
+public:
+	startButton()
+	{
+
+	}
+	startButton(string t, Vector2f size, int charSize, Color bgColor, Color textColor)
+	{
+		text.setString(t);
+		text.setFillColor(textColor);
+		text.setCharacterSize(charSize);
+
+		button.setSize(size);
+		button.setFillColor(bgColor);
+
+	}
+
+	void setFont(Font &font)
+	{
+		text.setFont(font);
+	}
+
+	void setBackColor(Color color)
+	{
+		button.setFillColor(color);
+	}
+
+	void setTextColor(Color color)
+	{
+		text.setFillColor(color);
+	}
+
+	void setPosition(Vector2f pos)
+	{
+		button.setPosition(pos);
+		float xPos = ((pos.x + button.getLocalBounds().width / 2.5) - (text.getLocalBounds().width / 2));
+		float yPos = ((pos.y + button.getLocalBounds().height / 4) - (text.getLocalBounds().height / 2));
+		text.setPosition({ xPos, yPos });
+	}
+
+	void drawTo(RenderWindow& window)
+	{
+		window.draw(button);
+		window.draw(text);
+	}
+
+	bool isMouseOver(RenderWindow& window)
+	{
+		float mouseX = Mouse::getPosition(window).x;
+		float mouseY = Mouse::getPosition(window).y;
+
+		float btnPosX = button.getPosition().x;
+		float btnPosY = button.getPosition().y;
+
+		float btnXPosWidth = button.getPosition().x + button.getLocalBounds().width;
+		float btnYPosHeight = button.getPosition().y + button.getLocalBounds().height;
+
+		if (mouseX < btnXPosWidth && mouseX > btnPosX && mouseY < btnYPosHeight && mouseY > btnPosY)
+		{
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+private:
+	RectangleShape button;
+	Text text;
+};
